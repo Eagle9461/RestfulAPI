39093a4936c9e9d37c66ab99c1245a891838c2c5
tank9995 <tank199995@outlook.com>
2023-03-27 06:18:43 -0700

    writed test script

--- main.js
+++ main.js
@@ -69,16 +69,35 @@ const generalSearch = (content) => {
     dataUrls.push('https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json');
     dataUrls.push('https://storage.googleapis.com/scratchpay-code-challenge/vet-clinics.json');
     
+    
     dataUrls.forEach( url => {
-            searchClinics(content.name, content.state, content.availability, url)
-                .then(data => {
-                    display(data);
-                })
-        }
+        searchClinics(content.name, content.state, content.availability, url)
+        .then(data => {
+            display(data);
+        })
+    }
     )
 }
 
-// Start
+const test = () => {
+    let testData = [   
+        {name:'Mount', state:'Cal', availability:{from:'12:00', to:''}, description:"Multiple terms"}, 
+        {name:'@\'\$\"\!Mount', state:'CA', availability:{from:'12:00', to:''}, description:"Character"},
+        {name:'', state:'', availability:{from:'', to:''},  description:"Empty"},
+        {name:'Mount', state:'Cal', availability:{from:'12:00', to:''}, description:"Valid terms"},
+        {name:'', state:'', availability:{from:'12', to:''},  description:"Invalid terms"},
+        {name:'Pets', state:'Kans', availability:{from:'', to:''}, description:"state searching"},
+        {name:' German', state:'KS', availability:{from:'', to:''}, description:"leading and trailing spaces"},
+        {name:'German Pets', state:'KS', availability:{from:'', to:''}, description:"spaces in between"}
+   ];
+    testData.forEach( (content, i) => {
+        setTimeout(
+            ()=> {
+                console.log(content.description + " -> name: "+content.name, "state:"+content.state, "availability: from:"+content.availability.from+" to:"+content.availability.to);
+                generalSearch(content);
+            }, 1000*i
+        )
+    })
+}
 
-generalSearch({name:'German Pets', state:'KS', availability:{from:'08:00', to:''}});
-
+test();
